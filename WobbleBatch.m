c606f145d36c307525c927e3e3060c122eca6d3c
Cassandra Donatelli <cassandra.donatelli@gmail.com>
2016-02-20 17:54:18 -0500

    Added timing

--- WobbleBatch.m
+++ WobbleBatch.m
@@ -1,15 +1,25 @@
-function [ list ] = WobbleBatch( file )
+function [ list, time] = WobbleBatch( file )
     load(file)
     clear file
     list = whos;
+    time = [];
     % a = imageInfo(eval(a(1).name))
     for i = 1:length(list)
+       t = [];
+       tic
        list(i).name = midlineRestructure(eval(list(i).name));
        disp(['MidRes ', num2str(i), ' out of ', num2str(length(list))]);
+       t = toc;
+       tic
        list(i).name = VidInfo(eval(list(i).name));
        disp(['VidInfo ', num2str(i), ' out of ', num2str(length(list))]);
+       t = [t;toc];
+       tic
        list(i).name = wobbleMax(eval(list(i).name));
        disp(['WobMax ', num2str(i), ' out of ', num2str(length(list))]);
+       t = [t;toc];
+       close all
+       time = [time, t];
     end
 
 end
