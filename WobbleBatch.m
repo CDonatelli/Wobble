fe54e0cc08df49ced732e588380b7825801864ff
Cassandra Donatelli <cassandra.donatelli@gmail.com>
2016-02-20 18:22:53 -0500

    fixed again

--- WobbleBatch.m
+++ WobbleBatch.m
@@ -7,19 +7,18 @@ function [ list, time] = WobbleBatch( file )
     for i = 1:length(list)
        t = [];
        tic
-       Temp = midlineRestructure(eval(list(i).name));
+       list(i).name = midlineRestructure(eval(list(i).name));
        disp(['MidRes ', num2str(i), ' out of ', num2str(length(list))]);
        t = toc;
        tic
-       Temp = VidInfo(eval(list(i).name));
+       list(i).name = VidInfo(list(i).name);
        disp(['VidInfo ', num2str(i), ' out of ', num2str(length(list))]);
        t = [t;toc];
        tic
-       Temp = wobbleMax(eval(list(i).name));
+       list(i).name = wobbleMax(list(i).name);
        disp(['WobMax ', num2str(i), ' out of ', num2str(length(list))]);
        t = [t;toc];
        close all
-       list(i).name = Temp;
        time = [time, t];
     end
 
