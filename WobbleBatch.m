425e3d8df094f99e1072a2865241401d8070f986
CDonatelli <cassandra.donatelli@tufts.edu>
2016-02-22 11:51:48 -0500

    Working on adding exception handling

--- WobbleBatch.m
+++ WobbleBatch.m
@@ -1,25 +1,37 @@
-function [ list, time] = WobbleBatch( file )
+function [ list, time, errors] = WobbleBatch( file )
     load(file)
     clear file
     list = whos;
     time = [];
     % a = imageInfo(eval(a(1).name))
     for i = 1:length(list)
-       t = [];
-       tic
-       list(i).name = midlineRestructure(eval(list(i).name));
-       disp(['MidRes ', num2str(i), ' out of ', num2str(length(list))]);
-       t = toc;
-       tic
-       list(i).name = VidInfo(list(i).name);
-       disp(['VidInfo ', num2str(i), ' out of ', num2str(length(list))]);
-       t = [t;toc];
-       tic
-       list(i).name = wobbleMax(list(i).name);
-       disp(['WobMax ', num2str(i), ' out of ', num2str(length(list))]);
-       t = [t;toc];
+           t = [];  tic
+       try
+           list(i).name = midlineRestructure(eval(list(i).name));
+           disp(['Ran MidRes ', num2str(i), ' out of ', num2str(length(list))]);
+       catch
+           warning(['midlineRestructure did not run for file #' num2str(i)])
+           errors.(eval(list(i).name)) = getReport(MEexception.last);
+       end
+           t = toc;  tic
+       try
+           list(i).name = VidInfo(list(i).name);
+           disp(['Ran VidInfo ', num2str(i), ' out of ', num2str(length(list))]);
+       catch
+           warning(['VidInfo did not run for file #' num2str(i)])
+           errors.(eval(list(i).name)) = getReport(MEexception.last);
+       end
+           t = [t;toc];  tic
+       try
+           list(i).name = wobbleMax(list(i).name);
+           disp(['Ran WobMax ', num2str(i), ' out of ', num2str(length(list))]);
+       catch
+           warning(['wobbleMax did not run for file #' num2str(i)])
+           errors.(eval(list(i).name)) = getReport(MEexception.last);
+       end
+           t = [t;toc];
        close all
-       time = [time, t];
+           time = [time, t];
     end
 
 end
